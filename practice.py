d502778794555849a5895c025e1568e0b44d90fe
AishwaryaRK <hanuman@Aishwaryas-MacBook-Air.local>
2025-02-24 15:51:41 +0530

    csv

---
+++ practice.py
@@ -0,0 +1,78 @@
+import collections
+import heapq
+from _datetime import datetime
+import logging
+
+
+class SalesAnalytics:
+    def __init__(self, filename: str):
+        self.revenue_sales = collections.defaultdict(lambda: collections.defaultdict(float))
+        self.product_quantity_map = collections.defaultdict(int)
+        self.transaction = collections.defaultdict(lambda: collections.defaultdict(list))
+        self.__aggregate_data(filename)
+
+    def __aggregate_data(self, filename: str):
+        try:
+            with open(filename, "r") as fd:
+                fd.readline()
+                for line in fd:
+                    data = line.split(",")
+                    timestamp = datetime.strptime(data[1], "%Y-%m-%d %H:%M:%S")
+                    month = timestamp.month
+                    product_id = data[2]
+                    category = data[3]
+                    price = float(data[4])
+                    quantity = float(data[5])
+                    location = data[7]
+                    payment_method = data[8]
+                    shipping_cost = float(data[9])
+
+                    self.revenue_sales[month][category] += price * quantity
+
+                    self.product_quantity_map[product_id] += quantity
+
+                    if payment_method in self.transaction[location]:
+                        self.transaction[location][payment_method][0] += (price * quantity + shipping_cost)
+                        self.transaction[location][payment_method][1] += 1
+                    else:
+                        self.transaction[location][payment_method] = [(price * quantity + shipping_cost), 1]
+
+
+        except FileNotFoundError as e:
+            logging.error("error: file not found %s", e)
+        except ValueError as e:
+            logging.error("error: invalid file content %s", e)
+        except Exception as e:
+            logging.error("error: %s", e)
+
+    def monthly_revenue_sales(self):
+        return self.revenue_sales
+
+    def top_k_products_sold(self, n: int):
+        max_heap = []
+        for product_id, quantity in self.product_quantity_map.items():
+            heapq.heappush(max_heap, (-quantity, product_id))
+
+        result = []
+        for i in range(n):
+            quantity, product_id = heapq.heappop(max_heap)
+            result.append((product_id, -quantity))
+        return result
+
+    def avg_transaction_by_payment_per_location(self):
+        for location, payment_transactions in self.transaction.items():
+            for payment, total_transaction in payment_transactions.items():
+                total_transaction_value = total_transaction[0]
+                count = total_transaction[1]
+                print(location, payment, total_transaction_value / count)
+
+
+sale_analytics = SalesAnalytics("sales_data.csv")
+# revenue_sales = sale_analytics.monthly_revenue_sales()
+# for month, categories in revenue_sales.items():
+#     for category, sales in categories.items():
+#         print(month, category, sales)
+#
+# print(sale_analytics.top_k_products_sold(5))
+
+sale_analytics.avg_transaction_by_payment_per_location()
--- tail-n.py
+++ tail-n.py
@@ -1,28 +1,55 @@
+import logging
 import os
 from typing import List
 
 
-def tail_n(file_name: str, n: int) -> List[str]:
-    with open(file_name, 'r') as fd:
-        fd.seek(0, os.SEEK_END)
-        file_size = position = fd.tell()
-        num_lines = 0
-        offset = 50  # 1024
-        i = 1
-        text = ""
-        while num_lines <= n and position > 0:
-            buffer_size = offset if file_size - offset * i >= 0 else file_size - offset * (i - 1)
-            seek_offset = file_size - buffer_size * i if file_size - offset * i >= 0 else 0
-            fd.seek(seek_offset, os.SEEK_SET)
-            buffer = fd.read(buffer_size)
-            # buffer = buffer.decode("utf-8") #for binary mode using -ve offset with SEEK_END
-            num_lines += buffer.count("\n")
-            text = buffer + text
-            i += 1
-            position = seek_offset + buffer_size
-        # lines.reverse()
-        lines = text.splitlines()
-        return lines[-n:]
+# def tail_n(file_name: str, n: int) -> List[str]:
+#     with open(file_name, 'r') as fd:
+#         fd.seek(0, os.SEEK_END)
+#         file_size = position = fd.tell()
+#         num_lines = 0
+#         offset = 50  # 1024
+#         i = 1
+#         text = ""
+#         while num_lines <= n and position > 0:
+#             buffer_size = offset if file_size - offset * i >= 0 else file_size - offset * (i - 1)
+#             seek_offset = file_size - buffer_size * i if file_size - offset * i >= 0 else 0
+#             fd.seek(seek_offset, os.SEEK_SET)
+#             buffer = fd.read(buffer_size)
+#             # buffer = buffer.decode("utf-8") #for binary mode using -ve offset with SEEK_END
+#             num_lines += buffer.count("\n")
+#             text = buffer + text
+#             i += 1
+#             position = seek_offset + buffer_size
+#         # lines.reverse()
+#         lines = text.splitlines()
+#         return lines[-n:]
 
 
-print(tail_n("test_file.txt", 5))
+def tail_n_lines(file_name: str, n: int) -> List[str]:
+    try:
+        with open(file_name, "r") as fd:
+            fd.seek(0, os.SEEK_END)
+            file_size = fd.tell()
+            offset = 50
+            position = file_size
+            n_lines = []
+            partial_line = ""
+            while len(n_lines) <= n and position >= 0:
+                buffer_size = min(offset, position)
+                position -= buffer_size
+                fd.seek(position, os.SEEK_SET)
+                buffer = fd.read(buffer_size)
+                buffer = buffer + partial_line
+                lines = buffer.splitlines()
+                partial_line = lines[0]
+                n_lines = lines[1:] + n_lines
+
+        return n_lines[-n:]
+    except FileNotFoundError as e:
+        logging.error("Error: file not found %s", e)
+    except Exception as e:
+        logging.error("Error: %s", e)
+
+
+print(tail_n_lines("test_file.txt", 6))
