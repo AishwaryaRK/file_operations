9aa1d38b15b24960338ae9b979c31d0b7493380e
AishwaryaRK <hanuman@Aishwaryas-MacBook-Air.local>
2025-02-27 09:24:07 +0530

    refactor

--- reverse_file.py
+++ reverse_file.py
@@ -6,29 +6,22 @@ def reverse(file_name: str, output_file_name: str):
         fd.seek(0, os.SEEK_END)
         file_size = fd.tell()
         offset = 50  # 1024
-        to_read = 0
-        file_read = False
-        i = 1
-        while not file_read:
-            buffer_size = offset if file_size - offset * i >= 0 else file_size - offset * (i - 1)
-            seek_offset = file_size - buffer_size * i if file_size - offset * i >= 0 else 0
-            if to_read:
-                buffer_size += to_read
-                to_read = 0
-            if seek_offset == 0:
-                file_read = True
-            fd.seek(seek_offset, os.SEEK_SET)
-            buffer = fd.read(buffer_size)
+        position = file_size
+        partial_line = ""
+        while position > 0:
+            buffer_size = min(offset, position)
+            position -= buffer_size
+            fd.seek(position, os.SEEK_SET)
+            buffer = fd.read(buffer_size) + partial_line
             lines = buffer.splitlines()
             lines = [line + "\n" for line in lines]
-            first_line = lines[0]
-            if not file_read:
-                lines = lines[1:]
-                to_read = len(first_line.encode("utf-8"))
+            partial_line = lines[0]
+            lines = lines[1:]
             lines.reverse()
             output_fd.writelines(lines)
             output_fd.flush()
-            i += 1
+        if partial_line:
+            output_fd.writelines(partial_line)
 
 
 reverse("test_file.txt", "reversed_file.txt")
